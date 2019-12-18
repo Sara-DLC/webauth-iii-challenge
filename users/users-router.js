a99e56728651f029bd8e10658b97ce1b0bfe1628
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-18 15:08:33 -0600

    migation completed

--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const { restricted, checkRole} = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, checkRole(""), (req, res) => {
+router.get("/", restricted, checkRole('finance'), (req, res) => {
     Users.find()
     .then(users => {
     res.json(users);
