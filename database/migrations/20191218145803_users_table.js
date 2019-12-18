5788aa1f5d1d280058f51f652f54b92a08e60dbe
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-18 15:11:19 -0600

    modified database, added dbconfig file

--- database/migrations/20191218145803_users_table.js
+++ database/migrations/20191218145803_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', table => {
         table.increments();
-        table.string('role', 128).notNullable();
+        table.string('department', 128).notNullable();
         table.string('username', 128).notNullable().unique();
         table.string('password', 128).notNullable();
     });
